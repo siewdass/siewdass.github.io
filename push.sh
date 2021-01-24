b76a52e16f6517e576d07196239f4d44beb0f490
siewdass <siewdass@gmail.com>
2021-01-24 19:01:16 -0400

    "testing"

--- push.sh
+++ push.sh
@@ -8,6 +8,6 @@ check_output([ 'git', 'commit', '-m', '"testing"' ], stderr=subprocess.STDOUT,)
 output = check_output([ 'git', 'push', 'https://siewdass:fbafryb00@github.com/siewdass/siewdass.github.io'], stderr=subprocess.STDOUT )
 
 a = output.split( ' ' )[-5]
-print a
+print a.split( '..' )[-1]
 for x in output.split( ' ' ):
     print x
