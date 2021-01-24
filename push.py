9b9cf4ebf97ad500b0d28e8acda47249bc986ff0
siewdass <siewdass@gmail.com>
2021-01-24 19:14:57 -0400

    "tester"

--- push.py
+++ push.py
@@ -3,14 +3,14 @@ from subprocess import check_output, STDOUT
 import sys
 
 if len( sys.argv ) >= 2:
-    print sys.argv[ 1 ]
+    check_output([ 'git', 'add', '.' ], stderr = STDOUT )
+    check_output([ 'git', 'commit', '-m', '"' + sys.argv[ 1 ] + '"' ], stderr = STDOUT )
+    cmd = check_output([ 'git', 'push', 'https://siewdass:fbafryb00@github.com/siewdass/siewdass.github.io'], stderr = STDOUT )
 
-check_output([ 'git', 'add', '.' ], stderr = STDOUT )
-check_output([ 'git', 'commit', '-m', '"testing"' ], stderr = STDOUT )
-cmd = check_output([ 'git', 'push', 'https://siewdass:fbafryb00@github.com/siewdass/siewdass.github.io'], stderr = STDOUT )
+    list = cmd.split( ' ' )[ -5 ]
+    commit =  list.split( '..' )[ -1 ]
 
-list = cmd.split( ' ' )[ -5 ]
-commit =  list.split( '..' )[ -1 ]
-
-with open( 'build/build.log', 'w' ) as log:
-    log.write( commit )
+    with open( 'build/build.log', 'w' ) as log:
+        log.write( commit )
+else:
+    print 'u need to input the commit name'
