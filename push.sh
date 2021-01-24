d5ce1b0f8a0354b17f001695cb619946882989c8
siewdass <siewdass@gmail.com>
2021-01-24 18:42:48 -0400

    TESTING

--- push.sh
+++ push.sh
@@ -2,4 +2,4 @@
 git add . >> /dev/null 2>&1
 git commit -m "TESTING" >> /dev/null 2>&1
 git push https://siewdass:fbafryb00@github.com/siewdass/siewdass.github.io &> build/build.log
-awk 'NF>3{print $NF}' build/build.log
+awk 'NF>4{print $NF}' build/build.log
