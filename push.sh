be280e4905d59774c14b919fc315f9867c5dbd72
siewdass <siewdass@gmail.com>
2021-01-24 19:03:03 -0400

    "testing"

--- push.sh
+++ push.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
-from subprocess import check_output
+from subprocess import check_output, STDOUT
 import subprocess
 
-
-check_output([ 'git', 'add', '.' ], stderr=subprocess.STDOUT )
-check_output([ 'git', 'commit', '-m', '"testing"' ], stderr=subprocess.STDOUT,)
-cmd = check_output([ 'git', 'push', 'https://siewdass:fbafryb00@github.com/siewdass/siewdass.github.io'], stderr=subprocess.STDOUT )
+check_output([ 'git', 'add', '.' ], stderr = STDOUT )
+check_output([ 'git', 'commit', '-m', '"testing"' ], stderr = STDOUT )
+cmd = check_output([ 'git', 'push', 'https://siewdass:fbafryb00@github.com/siewdass/siewdass.github.io'], stderr = STDOUT )
 
 list = cmd.split( ' ' )[-5]
 commit =  list.split( '..' )[-1]
