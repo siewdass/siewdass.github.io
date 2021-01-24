6eb7cb1099f25d791a9ef9720ef2e7cfd53dd2a7
siewdass <siewdass@gmail.com>
2021-01-24 18:09:11 -0400

    TESTING

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git add . >> /dev/null 2>&1
 git commit -m "TESTING" >> /dev/null 2>&1
-git push  https://siewdass:fbafryb00@github.com/siewdass/siewdass.github.io > build/build.log
+git push  https://siewdass:fbafryb00@github.com/siewdass/siewdass.github.io >> build/build.log
